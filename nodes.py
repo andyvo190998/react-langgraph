b67344b279319c2ffabc181dfaa723847d615350
andyvo190998 <andyvo190998@gmail.com>
2026-09-08 05:55:56 +0200

    add node

---
+++ nodes.py
@@ -0,0 +1,20 @@
+from dotenv import load_dotenv
+from langgraph.graph import MessagesState
+from langgraph.prebuilt import ToolNode
+
+from react import llm, tools
+
+load_dotenv()
+
+SYSTEM_MESSAGE="""
+you are a helpful assistant that can use tools to answer questions.
+"""
+
+def run_agen_reasoning(state: MessagesState) -> MessagesState:
+    """
+    Run the agent reasoning node.
+    """
+    response = llm.invoke([{"role": "system", "content": SYSTEM_MESSAGE}, *state["messages"]])
+    return {"messages": [response]}
+
+tool_node = ToolNode(tools)
